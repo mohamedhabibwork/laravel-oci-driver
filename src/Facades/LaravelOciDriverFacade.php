67cc82ad0229275b0967661c2d045135594dd437
Mohamed Habib <mohamedhabibwork@gmail.com>
2025-05-31 15:16:06 +0300

    feat: add LaravelOciDriverFacade for easier access to the LaravelOciDriver

--- src/Facades/LaravelOciDriverFacade.php
+++ src/Facades/LaravelOciDriverFacade.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \LaravelOCI\LaravelOciDriver\LaravelOciDriver
  */
-class LaravelOciDriver extends Facade
+class LaravelOciDriverFacade extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
